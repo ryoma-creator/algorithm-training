574ca0c8b75e96a1cc0d1a4df9640dc86820a903
Ryoma/Cross <ryoma111326@gmail.com>
2026-02-05 15:24:03 +0800

    feat: practice Map value-to-index lookup

--- day1_map.js
+++ day1_map.js
@@ -8,3 +8,13 @@ console.log(map.get("red"))
 
 const nums = [3,5,9];
 
+
+const numMap = new Map();
+const numbers1 =  [8, 12, 20];
+
+for(let i = 0; i < numbers1.length; i++) {
+  numMap.set(numbers1[i], i);
+}
+
+console.log(numMap.get(20));
+
